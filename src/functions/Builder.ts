fbaebff738b4b730463465ef654bbf3f675e39eb
chap0lin <carlosrochacaar@gmail.com>
2023-07-06 17:42:58 -0300

    adding builderTileMap function

--- src/functions/Builder.ts
+++ src/functions/Builder.ts
@@ -1,10 +1,13 @@
 import playerSprite from '../assets/player.png';
+import { coordinate } from "../types";
 import { Player } from '../classes';
 import {
   PLAYER_SIZE,
   PLAYER_SPEED,
   ANIMATION_PERIOD,
   PLAYER_FEET_OFFSET,
+  DX,
+  DY
 } from '../constants';
 
 
@@ -28,12 +31,25 @@ export const spawnPlayer = (origin) => {
   return players;
 };
 
+export function buildTileMap({x, y}: {x: number, y: number}): coordinate[][] {
+  const tileMap = []
+  for(let i = 0; i < x; i += 1) {
+    const row: coordinate[] = [];
+    for(let j = 0; j < y; j+= 1) {
+      row.push({x: (i+j)*DX, y: (i-j)*(DY)});
+    }
+    tileMap.push(row);
+  }
+  return tileMap;
+}
+
 
 export async function buildScene(s: number) {
   const {sceneOne} = await import("../gameLogic/levels/One");
   const {sceneTwo} = await import("../gameLogic/levels/Two");
+  const {sceneThree} = await import("../gameLogic/levels/Three");
 
-  const scenes = [sceneOne, sceneTwo];  //new scenes must be added to this array
+  const scenes = [sceneOne, sceneTwo, sceneThree];  //new scenes must be added to this array
 
   const objects = scenes[s].objects;
   const floor = scenes[s].floor;
