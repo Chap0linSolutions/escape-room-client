30b522a3a63162de4debce4610ae0feacaaeded8
chap0lin <carlosrochacaar@gmail.com>
2023-07-19 11:30:41 -0300

    adding hud components

---
+++ src/components/HUD/HUD.style.ts
@@ -0,0 +1,41 @@
+import styled from "@emotion/styled";
+
+export const RightSideContainer = styled.div({
+  position: 'absolute',
+  right: '16px',
+  top: '16px',
+  width: '140px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '24px'
+});
+
+export const OptionsContainer = styled.div({
+  background: '#404143',
+  height: '102px',
+  display: 'flex',
+  flexDirection: 'row',
+  gap: '10px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '20px'
+});
+
+export const InventoryContainer = styled.div({
+  padding: '16px',
+  height: '680px',
+  background: '#404143',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '16px',
+  borderRadius: '20px'
+});
+
+export const SlotContainer = styled.div({
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '8px'
+});
