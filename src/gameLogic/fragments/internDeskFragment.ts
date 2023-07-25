6e992787d7dd1c9ee3ba789d5734afef34de5d51
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2023-07-25 18:21:31 -0300

    Initial commit

---
+++ src/gameLogic/fragments/internDeskFragment.ts
@@ -0,0 +1,88 @@
+import { Sprite, Fragment, FragmentParams, InventoryItem } from '../../classes';
+import { InputHandler } from '../../events/InputHandler';
+import { State } from '../state';
+import deskSprite from '../../assets/fragments/internDeskFragment.png';
+//import soundFile from '../../assets/sounds/woosh1.mp3'; //TODO: change to switch sound
+import { coordinate } from '../../types';
+import { renderHitbox } from '../../functions/Metrics';
+import { SHOW_HITBOX } from '../../constants';
+
+export class internDeskFragment extends Fragment {
+    interactions;
+    sprite: Sprite;
+    lampOn = false;
+    calendarZoom = false;
+
+    constructor({ object }: FragmentParams) {
+        super({ object });
+        this.sprite = new Sprite({
+            sprite: deskSprite,
+            size: 600,
+            rows: 2,
+            columns: 2,
+        });
+
+        this.items = [];
+        this.interactions = {
+            calendar: { coordinate: { x: 70, y: 180 }, radius: 30 },
+            deskLamp: { coordinate: { x: 463, y: 147 }, radius: 20 },
+            bookPile: { coordinate: { x: 510, y: 350 }, radius: 50 },
+        };
+        const inputHandler = new InputHandler();
+        inputHandler.subscribe('mouseDown', 'deskFragmentMouseDown', (pos) =>
+            this.interact(pos)
+        );
+    }
+
+    interact(clickCoords: coordinate): void {
+        if (!this.isVisible()) return;
+        if (!this.interactions || !clickCoords) return;
+        const state = new State();
+        //const switchSound = new Sound({ source: soundFile });
+
+        if (!this.calendarZoom) {
+            if (this.isWithin(this.interactions.calendar, clickCoords)) {
+                this.calendarZoom = true;
+            } else if (this.isWithin(this.interactions.deskLamp, clickCoords)) {
+                this.lampOn = !this.lampOn;
+                //switchSound.play();
+            } else if (this.isWithin(this.interactions.bookPile, clickCoords)) {
+                state.cb.showToast({
+                    title: 'Livros',
+                    description: 'Uma pilha de livros da biblioteca da faculdade',
+                    backgroundColor: '#8c4b8c',
+                  });
+            }
+        } else {
+            this.calendarZoom = false;
+        }
+
+        return this.sprite.setQuad([this.calendarZoom ? 1 : 0, this.lampOn ? 1 : 0]);
+    }
+
+    drawHitboxes(canvas: CanvasRenderingContext2D) {
+        renderHitbox(
+            canvas,
+            this.getAbsoluteCoords(this.interactions.calendar.coordinate),
+            this.interactions.calendar.radius
+        );
+        renderHitbox(
+            canvas,
+            this.getAbsoluteCoords(this.interactions.deskLamp.coordinate),
+            this.interactions.deskLamp.radius
+        );
+        renderHitbox(
+            canvas,
+            this.getAbsoluteCoords(this.interactions.bookPile.coordinate),
+            this.interactions.bookPile.radius
+        );
+    }
+
+    render(canvas: CanvasRenderingContext2D): void {
+        const { width, height } = this.sprite.getAllDimensions();
+        !this.position && this.setPosition(width, height);
+        this.drawBackground(canvas, width, height, 2);
+        this.sprite.render(canvas, this.position);
+        SHOW_HITBOX && this.drawHitboxes(canvas);
+    }
+}
