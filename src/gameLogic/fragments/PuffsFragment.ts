ad6b6eafff46c3da1ab86c0bada7f77d59f0345a
Rafa Sinhoroto <rsinhoroto@yahoo.com>
2023-08-02 16:55:58 -0300

    Fixed code

--- src/gameLogic/fragments/PuffsFragment.ts
+++ src/gameLogic/fragments/PuffsFragment.ts
@@ -5,7 +5,8 @@ import { State } from '../../gameLogic/state';
 import Sound from '../../classes/Sound';
 import soundFile from '../../assets/sounds/woosh1.mp3';
 import puffsSprite from '../../assets/fragments/puffsPlantFragment.png';
-//import { renderHitbox } from '../../functions/Metrics';
+import { renderHitbox } from '../../functions/Metrics';
+import { SHOW_HITBOX } from '../../constants';
 
 export class PuffsFragment extends Fragment {
   interactions;
@@ -34,59 +35,80 @@ export class PuffsFragment extends Fragment {
     );
   }
 
-  update(dt: number): void {}
-
   interact(clickCoords: coordinate): void {
     if (!this.position) return;
     if (!this.interactions || !clickCoords) return;
     const state = new State();
     const plantSound = new Sound({ source: soundFile });
 
-    if (this.isWithin(this.interactions.plantVase, clickCoords)) {
-      this.plantVaseUp = !this.plantVaseUp;
+    if (this.plantVaseUp) {
+      this.plantVaseUp = false;
       plantSound.play();
-      return this.sprite.setQuad([0, this.plantVaseUp ? 1 : 0]);
+      this.sprite.setQuad([0, 0]);
     } else {
-      let title = 'Error';
-      let description = 'Something went wrong...';
-      let color = '#f0f0f0';
-
-      if (this.isWithin(this.interactions.bluePuff, clickCoords)) {
-        title = 'Pufe Azul';
-        description = 'Agora não é hora de descansar!';
-        color = '#0f0fff';
-      } else if (this.isWithin(this.interactions.redPuff, clickCoords)) {
-        title = 'Pufe Vermelho';
-        description = 'Agora não é hora de descansar!';
-        color = '#ff0f0f';
-      } else if (this.isWithin(this.interactions.whiteBoard, clickCoords)) {
-        title = 'Quadro branco';
-        description = 'Um quadro em branco...';
-        color = '#808080';
+      if (this.isWithin(this.interactions.plantVase, clickCoords)) {
+        this.plantVaseUp = true;
+        plantSound.play();
+        return this.sprite.setQuad([0, 1]);
       } else {
-        return;
-      }
+        let title = 'Error';
+        let description = 'Something went wrong...';
+        let color = '#f0f0f0';
 
-      state.cb.showToast({
-        title: title,
-        description: description,
-        backgroundColor: color,
-      });
+        if (this.isWithin(this.interactions.bluePuff, clickCoords)) {
+          title = 'Pufe Azul';
+          description = 'Agora não é hora de descansar!';
+          color = '#0f0fff';
+        } else if (this.isWithin(this.interactions.redPuff, clickCoords)) {
+          title = 'Pufe Vermelho';
+          description = 'Agora não é hora de descansar!';
+          color = '#ff0f0f';
+        } else if (this.isWithin(this.interactions.whiteBoard, clickCoords)) {
+          title = 'Quadro branco';
+          description = 'Um quadro em branco...';
+          color = '#808080';
+        } else {
+          return;
+        }
+
+        state.cb.showToast({
+          title: title,
+          description: description,
+          backgroundColor: color,
+        });
+      }
     }
   }
 
-  // drawHitboxes(canvas: CanvasRenderingContext2D) {
-  //     renderHitbox(canvas, this.getAbsoluteCoords(this.interactions.plantVase.coordinate), this.interactions.plantVase.radius);
-  //     renderHitbox(canvas, this.getAbsoluteCoords(this.interactions.bluePuff.coordinate), this.interactions.bluePuff.radius);
-  //     renderHitbox(canvas, this.getAbsoluteCoords(this.interactions.redPuff.coordinate), this.interactions.redPuff.radius);
-  //     renderHitbox(canvas, this.getAbsoluteCoords(this.interactions.whiteBoard.coordinate), this.interactions.whiteBoard.radius);
-  // }
+  drawHitboxes(canvas: CanvasRenderingContext2D) {
+    renderHitbox(
+      canvas,
+      this.getAbsoluteCoords(this.interactions.plantVase.coordinate),
+      this.interactions.plantVase.radius
+    );
+    renderHitbox(
+      canvas,
+      this.getAbsoluteCoords(this.interactions.bluePuff.coordinate),
+      this.interactions.bluePuff.radius
+    );
+    renderHitbox(
+      canvas,
+      this.getAbsoluteCoords(this.interactions.redPuff.coordinate),
+      this.interactions.redPuff.radius
+    );
+    renderHitbox(
+      canvas,
+      this.getAbsoluteCoords(this.interactions.whiteBoard.coordinate),
+      this.interactions.whiteBoard.radius
+    );
+  }
 
+  update(_dt: number) {}
   render(canvas: CanvasRenderingContext2D): void {
     const { width, height } = this.sprite.getAllDimensions();
     !this.position && this.setPosition(width, height);
     this.drawBackground(canvas, width, height, 2);
     this.sprite.render(canvas, this.position);
-    //this.drawHitboxes(canvas);
+    SHOW_HITBOX && this.drawHitboxes(canvas);
   }
 }
